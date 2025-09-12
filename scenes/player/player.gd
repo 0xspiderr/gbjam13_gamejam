8bb4f01707449e8f5e71527271791df205981589
Harry <harypatrascu.1@gmail.com>
2025-09-12 19:42:58 +0300

    comment

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -25,6 +25,7 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _move_player(delta: float) -> void:
 	player_dir = Input.get_vector("left", "right", "up", "down")
+	# velocity weight determines wether the player will accelerate or slow down based on input
 	var velocity_weight := 0.0
 	# normalize direction so that the player speed will be the same diagonally too.
 	player_dir = player_dir.normalized()
