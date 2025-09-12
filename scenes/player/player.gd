55b76797a3ca3427e726324c4a55c875bde9c2f1
Harry <harypatrascu.1@gmail.com>
2025-09-12 18:35:02 +0300

    added idle animation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,10 +5,15 @@ extends CharacterBody2D
 @export var speed: float = 50.0
 @export var friction: float = 4.5
 @export var acceleration: float = 12.5
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+var player_dir: Vector2 = Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
 	_move_player(delta)
+	_play_animation()
 	move_and_slide()
 
 
@@ -18,15 +23,22 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _move_player(delta: float) -> void:
-	var direction := Input.get_vector("left", "right", "up", "down")
+	player_dir = Input.get_vector("left", "right", "up", "down")
 	var velocity_weight := 0.0
 	# normalize direction so that the player speed will be the same diagonally too.
-	direction = direction.normalized()
+	player_dir = player_dir.normalized()
 	
-	if direction:
+	if player_dir:
 		velocity_weight = delta * acceleration
 	else:
 		velocity_weight = delta * friction
+	
+	velocity.x = lerpf(velocity.x, player_dir.x * speed, velocity_weight)
+	velocity.y = lerpf(velocity.y, player_dir.y * speed, velocity_weight)
 
-	velocity.x = lerpf(velocity.x, direction.x * speed, velocity_weight)
-	velocity.y = lerpf(velocity.y, direction.y * speed, velocity_weight)
+
+func _play_animation() -> void:
+	if player_dir.length() == 0:
+		animation_player.play(&"idle")
+	else:
+		animation_player.stop()
