8c0545c08a4cb343d98ebdbc0619f5d4ffa78fde
Harry <harypatrascu.1@gmail.com>
2025-09-13 17:16:39 +0300

    stopped play from walking when he is colliding with walls and other items

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,12 +3,12 @@ extends CharacterBody2D
 
 
 @export var speed: float = 100.0
-@export var friction: float = 4.5
-@export var acceleration: float = 12.5
 @onready var player_sprites: AnimatedSprite2D = $PlayerSprites
-
+var is_colliding: bool = false
 
 var player_dir: Vector2 = Vector2.ZERO
+var last_player_dir: Vector2 = Vector2.ZERO
+var last_position: Vector2
 
 
 func _ready() -> void:
@@ -20,34 +20,33 @@ func _physics_process(delta: float) -> void:
 	if PlayerData.is_in_combat:
 		return
 	
-	_move_player(delta)
 	_play_animation()
+	_move_player(delta)
 	move_and_slide()
 
 
 func _move_player(delta: float) -> void:
 	player_dir = Input.get_vector("left", "right", "up", "down")
-	# velocity weight determines wether the player will accelerate or slow down based on input
-	var velocity_weight := 0.0
-	# normalize direction so that the player speed will be the same diagonally too.
-	player_dir = player_dir.normalized()
-	
-	if player_dir:
-		velocity_weight = delta * acceleration
-	else:
-		velocity_weight = delta * friction
 	
 	if player_dir == Vector2(1, 0) or player_dir == Vector2(-1, 0):
 		velocity.x = player_dir.x * speed
+		last_position = position
 		velocity.y = 0
 	if player_dir == Vector2(0, 1) or player_dir == Vector2(0, -1):
 		velocity.y = player_dir.y * speed
 		velocity.x = 0
+		last_position = position
 	if player_dir not in [Vector2(0, 1), Vector2(0, -1), Vector2(1, 0),Vector2(-1, 0)]:
 		velocity = Vector2.ZERO
+		last_position = position
+	
 
 
 func _play_animation() -> void:
+	if last_position == position:
+		player_sprites.play(&"idle")
+		return
+	
 	var player_dir_len: float = player_dir.length()
 	
 	if player_dir_len == 0:
@@ -60,6 +59,12 @@ func _play_animation() -> void:
 		player_sprites.play(&"walk_right")
 	elif player_dir.x == -1:
 		player_sprites.play(&"walk_left")
-	else:
-		player_sprites.play(&"idle")
-		player_sprites.stop()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	is_colliding = true
+	last_player_dir = player_dir
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	is_colliding = false
