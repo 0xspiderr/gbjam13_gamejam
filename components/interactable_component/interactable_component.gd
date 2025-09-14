c6ba8116d8e3eaf4799e83df95f6fe6e31847470
Harry <harypatrascu.1@gmail.com>
2025-09-14 19:21:50 +0300

    interactable component done

--- components/interactable_component/interactable_component.gd
+++ components/interactable_component/interactable_component.gd
@@ -1,19 +1,31 @@
-extends Area2D
+class_name InteractableComponent
+extends StaticBody2D
 
 
-@onready var interactable_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = %AnimatedSprite
 @export var interactable_stats: InteractableStats
+@onready var area_shape: CollisionShape2D = %AreaShape
+@onready var collision_shape: CollisionShape2D = %CollisionShape
+
 
 
 func _ready() -> void:
 	# when a new interactable spawns get the first animation from it's resource
 	# which is the idle animation for every npc and play it.
 	var idle_anim = interactable_stats.sprite_frames.get_animation_names()[0]
-	interactable_sprite.sprite_frames = interactable_stats.sprite_frames
-	interactable_sprite.animation = idle_anim
-	interactable_sprite.play(idle_anim)
+	animated_sprite.sprite_frames = interactable_stats.sprite_frames
+	animated_sprite.animation = idle_anim
+	animated_sprite.play(idle_anim)
+	
+	# change the area radius size based on the spawned object
+	var new_area_shape = RectangleShape2D.new()
+	new_area_shape.size = interactable_stats.area_size
+	area_shape.shape = new_area_shape
+	var new_col_shape = RectangleShape2D.new()
+	new_col_shape.size = interactable_stats.collision_size
+	collision_shape.shape = new_col_shape
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_interactable_component_area_body_entered(body: Node2D) -> void:
 	if body is Player:
 		print("player entered interactable area")
--- resources/interactable/interactable_stats.gd
+++ resources/interactable/interactable_stats.gd
@@ -4,3 +4,5 @@ extends Resource
 
 @export var name: String
 @export var sprite_frames: SpriteFrames
+@export var area_size: Vector2
+@export var collision_size: Vector2
