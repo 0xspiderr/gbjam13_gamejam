5649df377c1f9031a0021ba418abd28331b844c4
Ddaerb <varga.alexandra.maria.239@gmail.com>
2025-09-22 00:34:41 +0300

    added script to disclaimer

---
+++ ui/disclaimer_scene/disclaimer_scene.gd
@@ -0,0 +1,7 @@
+extends Control
+
+const MAIN_MENU = preload("uid://bgasbu3xvl0ur")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		get_tree().change_scene_to_packed(MAIN_MENU)
